393e5dff0340448f11d91e1aff21749b1b474adb
Denis Korolev <deniskorolev@Deniss-MacBook-Pro.local>
2024-11-23 23:11:49 +0300

    added field fs_uniquifier

--- models.py
+++ models.py
@@ -74,6 +74,7 @@ class User(db.Model, UserMixin):
     user_bio = db.Column(db.Text, nullable=True)
     user_avatar = db.Column(db.LargeBinary, nullable=True)
     active = db.Column(db.Boolean, default=True, nullable=False)
+    fs_uniquifier = db.Column(db.String(255), unique=True, nullable=True)
 
     user_to_profiles = db.relationship('UserProfile', lazy="joined", backref=db.backref("profile_to_user"), cascade="all, delete-orphan")
     user_to_reports = db.relationship('Report', lazy=True)
